f9598b7942c2b381b8ac2dfa12a6f39f21969e18
alones-dev <kyliandaumont78@gmail.com>
2024-05-16 21:23:26 +0200

    cpp-02

---
+++ cpp-02/ex00/Fixed.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 21:52:50 by kdaumont          #+#    #+#             */
+/*   Updated: 2024/05/16 20:52:30 by kdaumont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &cpy) : _value(cpy._value)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &rhs)
+{
+    std::cout << "Copy assignement operator called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
---
+++ cpp-02/ex00/Fixed.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 21:52:50 by kdaumont          #+#    #+#             */
+/*   Updated: 2024/05/16 19:30:18 by kdaumont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    private:
+        int _value;
+        static const int _bits = 8;
+    public:
+        Fixed();
+        Fixed(Fixed const &cpy);
+        ~Fixed();
+        
+        Fixed &operator=(Fixed const &rhs);
+        
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+};
+
+
+#endif
