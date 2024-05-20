8740476e17ab5c574cb6367c208abcea510e7d95
Kylian Daumont <kdaumont@student.42.fr>
2024-05-20 14:28:22 +0200

    fix

--- cpp-01/ex01/main.cpp
+++ cpp-01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 21:52:50 by kdaumont          #+#    #+#             */
-/*   Updated: 2024/05/16 12:47:18 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:27:56 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 int main(void)
 {
     std::string input;
+    int nb = 15;
 
     std::cout << "\033[1;36mEnter the name of the horde: \033[0m";
     getline(std::cin, input);
     if (std::cin.fail())
         return (1);
         
-    Zombie *horde = zombieHorde(15, input);
+    Zombie *horde = zombieHorde(nb, input);
     if (!horde)
     {
         std::cerr << "\033[1;31mError: The horde could not be created.\033[0m" << std::endl;
         return (1);
     }
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < nb; i++)
         horde[i].announce();
     delete [] horde;
     return (0);
