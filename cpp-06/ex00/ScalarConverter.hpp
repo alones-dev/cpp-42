1658159ce8d0bf3ed9d6a40c30c7c87d35aac836
alones-dev <kyliandaumont78@gmail.com>
2024-06-09 18:36:11 +0200

    cpp06 init

---
+++ cpp-06/ex00/ScalarConverter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 18:33:43 by kdaumont          #+#    #+#             */
+/*   Updated: 2024/06/09 18:35:27 by kdaumont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+
+};
+
+#endif
