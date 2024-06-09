66b48571a439a8b3b58eb660063237156d913ab9
alones-dev <kyliandaumont78@gmail.com>
2024-06-09 18:26:08 +0200

    ex03

--- cpp-05/ex03/Intern.cpp
+++ cpp-05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:45:07 by kdaumont          #+#    #+#             */
-/*   Updated: 2024/06/07 15:58:13 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/06/09 18:21:26 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,23 @@ Intern &Intern::operator=(Intern const &rhs)
 
 AForm* Intern::makeForm(std::string const form, std::string const target)
 {
+	std::string list[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+	AForm* forms[3] = {new ShrubberyCreationForm(target), new RobotomyRequestForm(target), new PresidentialPardonForm(target)};
+	AForm* final = NULL;
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (form == list[i])
+		{
+			final = forms[i];
+			std::cout << "Intern creates " << form << std::endl;
+		}
+		else
+			delete forms[i];
+	}
+
+	if (!final)
+		std::cout << "Intern cannot create " << form << " because it does not exist" << std::endl;
+
+	return final;
 }
--- cpp-05/ex03/Intern.hpp
+++ cpp-05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:45:17 by kdaumont          #+#    #+#             */
-/*   Updated: 2024/06/07 15:55:36 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/06/09 18:19:42 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 #include <iostream>
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 class AForm;
 
@@ -27,7 +30,7 @@ class Intern
 
 		Intern &operator=(Intern const &rhs);
 
-		AForm* makeForm(std::string const form, std::string const target);
+		AForm* makeForm(std::string form, std::string target);
 };
 
 #endif
