02596f5ab444780af3380c8c56c6b26083c92e69
Kylian Daumont <kdaumont@student.42.fr>
2024-06-14 12:52:57 +0200

    ex02

--- cpp-06/ex02/main.cpp
+++ cpp-06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 11:17:01 by kdaumont          #+#    #+#             */
-/*   Updated: 2024/06/14 11:43:46 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:52:40 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,24 @@
 
 Base* generate(void)
 {
+	Base* base = NULL;
 	srand(time(0));
 	int rdm = rand() % 3;
 
 	switch (rdm)
 	{
 		case 0:
-			return new A;
+			base = new A;
+			break;
 		case 1:
-			return new B;
+			base = new B;
+			break;
 		case 2:
-			return new C;
+			base = new C;
+			break;
 	}
+	
+	return base;
 }
 
 void identify(Base* p)
@@ -49,23 +55,36 @@ void identify(Base& p)
 	{
 		A &a = dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;	
+		(void)a;
 	}
-	catch(std::bad_cast &e) {}
+	catch(std::exception &e) {}
 	try 
 	{
 		B &b = dynamic_cast<B&>(p);
 		std::cout << "B" << std::endl;	
+		(void)b;
 	}
-	catch(std::bad_cast &e) {}
+	catch(std::exception &e) {}
 	try 
 	{
 		C &c = dynamic_cast<C&>(p);
 		std::cout << "C" << std::endl;	
+		(void)c;
 	}
-	catch(std::bad_cast &e) {}
+	catch(std::exception &e) {}
 }
 
 int main()
 {
+	srand(time(0));
+
+	Base* b1 = generate();
+
+	std::cout << "Identify b1" << std::endl;
+	identify(b1);
+	identify(*b1);
+	
+	delete b1;
+
 	return 0;
 }
